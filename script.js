9cd3df928f7faa37625da09bf4dc58a0cc000a83
Janhavi <janhavi25verma@gmail.com>
2022-10-05 14:56:11 +0530

    cloud animation

--- script.js
+++ script.js
@@ -28,3 +28,46 @@ function rabbit() {
 }
 // tortoise();
 // rabbit();
+// -------------------Clouds---------------------------
+function cloud1() {
+  let cloud1 = document.querySelector("#cloud1");
+  let start = 20;
+  let end = 800;
+  let wind = 9;
+  let skyclouds = setInterval(function () {
+    start = start + wind;
+    if (start >= end) {
+      start = 0;
+    }
+    cloud1.style.left = start + "px";
+  }, 90);
+}
+function cloud2() {
+  let cloud2 = document.querySelector("#cloud2");
+  let start = 500;
+  let end = 800;
+  let wind = 9;
+  let skyclouds = setInterval(function () {
+    start = start + wind;
+    if (start >= end) {
+      start = 0;
+    }
+    cloud2.style.left = start + "px";
+  }, 90);
+}
+function cloud3() {
+  let cloud3 = document.querySelector("#cloud3");
+  let start = 600;
+  let end = 800;
+  let wind = 9;
+  let skyclouds = setInterval(function () {
+    start = start + wind;
+    if (start >= end) {
+      start = 0;
+    }
+    cloud3.style.left = start + "px";
+  }, 90);
+}
+cloud1();
+cloud2();
+cloud3();
