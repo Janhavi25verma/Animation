7d6134d5001b898e15cecf262809f484cf88b843
Janhavi <janhavi25verma@gmail.com>
2022-10-05 11:48:27 +0530

    Added Buuton and styled

--- script.js
+++ script.js
@@ -3,27 +3,28 @@ function tortoise() {
   let tortoise = document.querySelector("#tortoise");
   let start = 20;
   let win = 940;
-  let tortoiseSpeed = 4;
+  let tortoiseSpeed = 3;
   let getsetgo = setInterval(function () {
     start = start + tortoiseSpeed;
     if (start >= win) {
       clearInterval(getsetgo);
     }
     tortoise.style.left = start + "px";
-  }, 200);
+  }, 100);
 }
+// ----------------rabbit------------------------
 function rabbit() {
   let rabbit = document.querySelector("#rabbit");
   let start = 20;
   let mid = 440;
-  let tortoiseSpeed = 8;
+  let tortoiseSpeed = 5;
   let getsetgo = setInterval(function () {
     start = start + tortoiseSpeed;
     if (start >= mid) {
       clearInterval(getsetgo);
     }
     rabbit.style.left = start + "px";
-  }, 200);
+  }, 100);
 }
-tortoise();
-rabbit();
+// tortoise();
+// rabbit();
